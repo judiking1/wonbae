44d0098bad4308cb71ec7656099c65ff30899fbc
wonbae <judiking1@naver.com>
2025-09-18 01:12:02 +0900

    update main ui

---
+++ app/_components/TagSection.tsx
@@ -0,0 +1,31 @@
+import { Card, CardHeader, CardTitle, CardContent, CardDescription } from '@/components/ui/card';
+import { NotionTag } from '@/types/notion';
+import Link from 'next/link';
+
+interface TagSectionProps {
+  tags: NotionTag[];
+}
+
+function TagSection({ tags }: TagSectionProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>태그 목록</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-col gap-3">
+          {tags.map((tag) => (
+            <Link href={`?tag=${tag.name}`} key={tag.name}>
+              <div className="hover:bg-muted-foreground/10 text-muted-foreground flex items-center justify-between rounded-md p-1.5 text-sm transition-colors">
+                <span>{tag.name}</span>
+                <span>{tag.count}</span>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default TagSection;
